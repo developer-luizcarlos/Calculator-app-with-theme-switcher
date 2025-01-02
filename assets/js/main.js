8b0d8ebff920115d6429331207101d81ca2b1d8d
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-01-02 09:40:40 -0300

    feat: taking some html tags in js file to create the initial and simple features

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+// DOM elements
+const $calcScreen = document.querySelector("#calc__screen");
+const $calcResetKey = document.querySelector("#calc__resetKey");
+const $calcResultKey = document.querySelector("#calc__resultKey");
+const $calcNumberKeys = Array.from(
+  document.querySelectorAll(".calc__numberKey")
+);
+const $calcOperationalKeys = Array.from(
+  document.querySelectorAll(".calc__operationalKey")
+);
